23a48572519085347213cb5b91773750ba5f0832
Amin Roslan <amin@VaseAdmins-MacBook-Pro.local>
2018-11-13 13:53:06 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const Data = require("./data");
+
+const API_PORT = 3001;
+const app = express();
+const router = express.Router();
+
+//MongoDB database
+const 
+
