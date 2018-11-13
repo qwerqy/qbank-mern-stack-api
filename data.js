1d64833aed7e1a736034cf57fcdf9858f9b8736c
Amin Roslan <amin@VaseAdmins-MacBook-Pro.local>
2018-11-13 15:15:31 +0800

    data js initial state

---
+++ data.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const DataSchema = new Schema({}, { timestamps: true });
+
+module.exports = mongoose.model("Data", DataSchema);
