db13e2cb2cdd08ebdcfe13caa0e16b3b3c362414
Amin Roslan <amin@VaseAdmins-MacBook-Pro.local>
2018-11-13 14:51:30 +0800

    server js to test database connection

--- server.js
+++ server.js
@@ -4,10 +4,21 @@ const bodyParser = require("body-parser");
 const logger = require("morgan");
 const Data = require("./data");
 
+require('dotenv').config()
+
 const API_PORT = 3001;
 const app = express();
 const router = express.Router();
 
 //MongoDB database
-const 
+const dbRoute = 'mongodb+srv://qwerqy:amin1234@qbank-vldip.azure.mongodb.net/test?retryWrites=true'
 
+//Connect back end to database
+mongoose.connect(
+    dbRoute,
+    { useNewUrlParser: true }
+)
+
+let db = mongoose.connection;
+
+db.once("open", () => console.log('database connected'))
